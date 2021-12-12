84403157b2f8dadc6496fe1141bed130fd1ad6e0
oov <ilovevipper@gmail.com>
2021-12-13 06:00:52 +0900

    cleanup

--- src/cmake/makelib.cmake
+++ src/cmake/makelib.cmake
@@ -2,19 +2,17 @@ cmake_minimum_required(VERSION 3.0.0)
 
 find_program(GENDEF gendef REQUIRED CMAKE_FIND_ROOT_PATH_BOTH)
 find_program(DLLTOOL dlltool REQUIRED CMAKE_FIND_ROOT_PATH_BOTH)
-find_program(LUA51 lua51.dll REQUIRED CMAKE_FIND_ROOT_PATH_BOTH)
 
-# Generate lua51.def
 execute_process(
-  COMMAND ${GENDEF} ${LUA51}
+  COMMAND ${GENDEF} - ${dll}
   WORKING_DIRECTORY "${local_dir}"
+  OUTPUT_FILE ${def}
   ERROR_QUIET
 )
 
 execute_process(
-  COMMAND ${DLLTOOL} -d lua51.def -y liblua51dl.a
+  COMMAND ${DLLTOOL} -d ${def} -D ${dll} -l ${lib} -y ${delayed_lib}
   WORKING_DIRECTORY "${local_dir}"
-  OUTPUT_VARIABLE _git_revision
   ERROR_QUIET
   OUTPUT_STRIP_TRAILING_WHITESPACE
 )
